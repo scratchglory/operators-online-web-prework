83e46ea190b139df2d95e92b4fd8f6d4d7b8bce4
Adella <adellakenn@gmail.com>
2019-04-24 19:42:20 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,6 +1,8 @@
 def unsafe?(speed)
-  speed > 60 ? true : false
-  speed < 40 ? true : false
+  # speed > 60 ? true : false
+  # speed < 40 ? true : false
+  puts true if speed > 60
+  puts true if speed < 40
 end
 
 
