34ff2b7b43cb5f23a89273243ab9aec18e1aab84
Adella <adellakenn@gmail.com>
2019-04-24 19:50:45 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -14,7 +14,7 @@ def not_safe?(speed)
 	speed > 60 ? true : false   # returns true if speed is greater than 60
 	speed < 40 ? true : false  # returns true if speed is less than 40
 	(speed < 60) && (speed > 40) ? false : true    # returns false if the speed is between 40 and 60
-	end
+	
 end
 	
 
